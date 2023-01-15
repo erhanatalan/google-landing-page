cd6fa93fab599bf52e75adee4e476c681c58175b
erhanatalan <erhanatalan2013@gmail.com>
2023-01-15 20:34:39 +0300

    Readme added

---
+++ README.md
@@ -0,0 +1,27 @@
+# GOOGLE LANDING PAGE (HTML+CSS)
+
+![gif](./img/google_landing_page.gif)
+
+## Description
+This project was created as a Google landing page.
+
+## Project Skeleton 
+
+```
+GOOGLE-LANDING-PAGE(folder)
+|
+|----README.md
+|----Img (folder)
+|----index.html
+|----style.css
+|----action.js
+
+
+## What I used in this project?
+
+- ## Html
+- ## Css
+  - ### Display
+  - ### Hover
+
+<left> ⌛ Happy Coding  💻🎶🍕🍀✍ </left>
--- action.js
+++ action.js
@@ -1,22 +1,3 @@
-// let n = Number(prompt("bir sayı giriniz"));
-// for (let j = 1; j<=n; j++) {
-//   const randomNum = Math.round(Math.random()*100)
-//   console.log(`${j}. random number: ${randomNum}`);
-// };
-let n = Number(prompt("enter a number"));
-let isPrime = true;
-if(n<2){
-    console.log("Number should be bigger than 1");
-}else{
-    for (let i = 2; i < n; i++) {
-    if( n % i == 0){
-        console.log(`${n} is not prime number`)
-        isPrime= false;
-        break
-    }
-    if (isPrime === true){
-    console.log(`${n} is prime number`)}
-    }
-}
+
 
 
